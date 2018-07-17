56caf670121b4c1c10a33b7ddf240db9ae8ef116
644356880@qq.com <gzy@gaozhengyudeMacBook-Air.local>
2018-07-17 20:13:01 +0800

    123

--- miao/lodash/gaozhengyu666-lodash.js
+++ miao/lodash/gaozhengyu666-lodash.js
@@ -1,7 +1,7 @@
 var gaozhengyu666 = {
   chunk: function(array, size) {
             var ary = []
-  			for(var i = 0; i < array.lenth ; i+=size) {
+  			for(var i = 0; i < array.length ; i+=size) {
   				ary.push(array.slice(i,size + i))
   			}
   			return ary
