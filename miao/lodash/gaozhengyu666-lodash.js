f5af14f0f757e511beceb9e5d505e97ddc27ea23
gzy <644356880@qq.com>
2018-07-18 11:18:46 +0800

    0718

--- miao/lodash/gaozhengyu666-lodash.js
+++ miao/lodash/gaozhengyu666-lodash.js
@@ -1,10 +1,69 @@
 var gaozhengyu666 = {
   chunk: function(array, size) {
-            var ary = []
-  			for(var i = 0; i < array.length ; i+=size) {
-  				ary.push(array.slice(i,size + i))
-  			}
-  			return ary
-
+   var ary = []
+   for(var i = 0; i < array.length ; i+=size) {
+    ary.push(array.slice(i,size + i))
   }
+  return ary
+},
+compact: function(array) {
+  var ary = []
+  var arraylegnth = array.length
+  for (var i = 0; i <arraylegnth; i++){
+    if(array[i]) {
+      ary.push(array[i])
+    }
+  }
+  return ary
+},
+difference: function(array,...theAgrs) {
+  var newArr=[]
+  var bigArr = [].concat(...theAgrs)
+  for (var i=0; i < array.length; i++) {
+    if (!bigArr.includes(array[i])) {
+      newArr.push(array[i])
+    }
+  }
+  return newArr
+    
+  
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
